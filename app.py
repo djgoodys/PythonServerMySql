33265b2a097ab1da98701589694e51243206295a
djgoodys <djgoodys@gmail.com>
2025-12-24 14:24:20 -0800

    fixing cors

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_mysqldb import MySQL
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
+#CORS(app)  # Enable CORS for all routes
+CORS(app, origins=["http://localhost:3000"])
 app.config['MYSQL_USER'] = 'avnadmin'
 app.config['MYSQL_PASSWORD'] = 'AVNS_tnJimgAkaMP36guFO_1'
 app.config['MYSQL_DB'] = 'defaultdb'
